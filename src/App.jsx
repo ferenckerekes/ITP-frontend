bb6a6fcae4438e6060a9571a98cf12a5321c7fb0
Ferenc Kerekes Jr <ferenckerekes9@gmail.com>
2023-04-05 09:05:23 +0300

    fetch users

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <h1>Good morning Vietnam</h1>;
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/");
+      console.log(response.data);
+      setUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+    console.log("Fetching users");
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  return (
+    <div>
+      <h1>Good morning Vietnam</h1>
+      <h2>Users</h2>
+
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.firstName} {user.lastName} ({user.email})
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
