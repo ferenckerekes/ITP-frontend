bd50074ff3474096e45d679134dfc74128ad4ab1
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2023-04-08 10:54:36 +0300

    Users Refactor & Introduced Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Users from "./components/Users";
+
 function App() {
   return (
     <div>
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,36 +1,11 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import UsersTable from "./UsersTable";
-import Alert from "@mui/material/Alert";
+import UsersProvider from "./UsersProvider";
 
 const Users = () => {
-  const [users, setUsers] = useState([]);
-  const [error, setError] = useState(null);
-
-  const fetchUsers = async () => {
-    try {
-      setError(null);
-      const response = await axios.get("http://localhost:3000/users");
-      setUsers(response.data);
-    } catch (error) {
-      console.error(error.message);
-      setError(error.message);
-    }
-  };
-
-  const refresh = () => {
-    fetchUsers();
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
   return (
-    <div>
-      {error && <Alert severity="error">{error}</Alert>}
-      <UsersTable users={users} refresh={refresh} />
-    </div>
+    <UsersProvider>
+      <UsersTable />
+    </UsersProvider>
   );
 };
 
